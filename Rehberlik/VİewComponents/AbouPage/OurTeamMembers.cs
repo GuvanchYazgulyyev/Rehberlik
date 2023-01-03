ec78498725fcf8d3ba904ef388600b21b129eb36
Guvanch Yazgulyyev <77998139+GuvanchYazgulyyev@users.noreply.github.com>
2023-01-04 00:52:42 +0300

    Hakkımızda Sayfası Bitti

---
+++ Rehberlik/VİewComponents/AbouPage/OurTeamMembers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rehberlik.Models.SqlDat;
+
+namespace Rehberlik.VİewComponents.AbouPage
+{
+    public class OurTeamMembers:ViewComponent
+    {
+        Context dr = new Context();
+        public IViewComponentResult Invoke()
+        {
+            var result = dr.Teams.OrderByDescending(k => k.Id).ToList();
+            return View(result);
+        }
+    }
+}
---
+++ Rehberlik/VİewComponents/ManiPage/ThreeStepsManiPage.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rehberlik.Models.SqlDat;
+
+namespace Rehberlik.VİewComponents.ManiPage
+{
+    public class ThreeStepsManiPage:ViewComponent
+    {
+        Context dr = new Context();
+        public IViewComponentResult Invoke()
+        {
+            var result = dr.ThreeSteps.Where(x => x.IsDelate == false).OrderByDescending(k => k.Id).ToList();
+            return View(result);
+        }
+    }
+}
